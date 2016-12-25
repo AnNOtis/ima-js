0e457cf1e84d267a8f9a19a0b95e61a84c655eea
Otis Chou <altispec@gmail.com>
2016-12-25 12:02:59 +0800

    add test

--- test/index.js
+++ test/index.js
@@ -28,6 +28,36 @@ test('ima("unknow-id") - with unknown element id', t => {
   }, /Can't find the element with id "unknow-id"/)
 })
 
+test('ima(Image) - with image element', t => {
+  prepareDocument('<img />')
+  const image = document.getElementsByTagName('img')[0]
+  t.is(ima(image).type, IMAGE_TYPE)
+  t.is(ima(image).origin, image)
+})
+
+test('ima(Canvas) - with canvas element', t => {
+  prepareDocument('<canvas />')
+  const canvas = document.getElementsByTagName('canvas')[0]
+  t.is(ima(canvas).type, CANVAS_TYPE)
+  t.is(ima(canvas).origin, canvas)
+})
+
+test('ima() - without image or canvas element', t => {
+  prepareDocument('<div />')
+  const elem = document.getElementsByTagName('div')[0]
+  t.throws(
+    () => ima(elem),
+    /Initial element should be HTMLImageElement or HTMLCanvasElement/
+  )
+})
+
+test('ima({width, height}) - create canvas', t => {
+  const size = {width: 200, height: 300}
+  t.is(ima(size).type, CANVAS_TYPE)
+  t.is(ima(size).origin.width, size.width)
+  t.is(ima(size).origin.height, size.height)
+})
+
 function prepareDocument(content) {
   global.document = jsdom.jsdom(content).defaultView.document
 }
